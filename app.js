7b106bb5085b3fbb91a48b407a65275fb470df70
Candyman770 <isaac2311.cl@gmail.com>
2020-05-27 13:15:21 +0530

    Passport Cookies

--- app.js
+++ app.js
@@ -3,9 +3,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session=require('express-session');
+const FileStore=require('session-file-store')(session);
+const passport=require('passport');
+const authenticate=require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var dishRouter=require('./routes/dishRouter');
+var leaderRouter=require('./routes/leaderRouter');
+var promoRouter=require('./routes/promoRouter');
+const mongoose=require('mongoose');
+const url='mongodb://localhost:27017/conFusion';
+const connect=mongoose.connect(url);
+
+connect.then((db)=>{
+	console.log('Connected correctly to the server');
+},err=>{console.log(err);});
 
 var app = express();
 
@@ -16,11 +30,81 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(cookieParser('12345-67890-09876-53421'));
+app.use(session({
+	name:'session-id',
+	secret:'12345-67890-09876-53421',
+	resave:false,
+	saveUninitialized:false,
+	store:new FileStore()
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+/*const auth=(req,res,next)=>{
+	console.log(req.signedCookies);
+
+	if(!req.signedCookies.user){
+		const authHeader=req.headers.authorization;
+		if(!authHeader){
+			const err=new Error('You are not Authenticated');
+			res.setHeader('WWW-Authenticate','Basic');
+			err.status=401;
+			return next(err);
+		}
+		const Auth=new Buffer.from(authHeader.split(' ')[1],'base64').toString().split(':');
+		const username=Auth[0];
+		const password=Auth[1];
+		if(username==='admin'&&password==='password'){
+			res.cookie('user','admin',{signed:true})
+			next();
+		}
+		else{
+			const err=new Error('You are not Authenticated');
+			res.setHeader('WWW-Authenticate','Basic');
+			err.status=401;
+			return next(err);
+		}
+	}
+	else{
+		if(req.signedCookies.user==='admin'){
+			next();
+		}
+		else{
+			const err=new Error('You are not Authenticated');
+			err.status=401;
+			return next(err);
+		}
+	}
+	
+}*/
+
+const auth=(req,res,next)=>{
+
+	if(!req.user){
+		const err=new Error('You are not Authenticated');
+		err.status=403;
+		return next(err);
+	}
+	else{
+		next();
+	}
+	
+};
+
+
+app.use(auth)
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/dishes',dishRouter);
+app.use('/promotions',promoRouter);
+app.use('/leaders',leaderRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ models/users.js
@@ -0,0 +1,13 @@
+const mongoose=require('mongoose');
+const schema=mongoose.Schema;
+const passportLocalMongoose=require('passport-local-mongoose');
+
+const user=new schema({
+	admin:{
+		type:Boolean,
+		default:false
+	}
+});
+
+user.plugin(passportLocalMongoose);
+module.exports=mongoose.model('User',user);
